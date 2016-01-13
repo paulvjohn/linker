fd5c5a1e25288cf56efc51207da8f18e10ef422e
Paul V John <mi0112@Myntras-MacBook-Pro-3.local>
2016-01-13 16:51:04 +0530

    fixed

--- php/send.php
+++ php/send.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -16,23 +15,4 @@ echo "Your email was sent!"; // success message
 echo "Invalid Email, please provide an correct email.";
 }
 
-=======
-<?php 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$subject = $_POST['subject'];
-
-$to = 'hello@gmail.com';
-$message = 'FROM: '.$name.' Email: '.$email.'Message: '.$message;
-$headers = 'From: youremail@domain.com' . "\r\n";
- 
-if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // this line checks that we have a valid email address
-mail($to, $subject, $message, $headers); //This method sends the mail.
-echo "Your email was sent!"; // success message
-}else{
-echo "Invalid Email, please provide an correct email.";
-}
-
->>>>>>> origin/gh-pages
 ?>
